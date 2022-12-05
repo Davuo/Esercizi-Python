9d5871f1549c55f168e5f8ffcaa3ffe8b476df2a
Davuo <losurdodavide056@gmail.com>
2022-12-05 20:53:59 +0100

    firstwindow

---
+++ etàbiglietto.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+window=tk.Tk()
+
+lblbiglietto=tk.Label(text="Costo del biglietto",
+            fg="white",
+            bg="gray").pack()
+
+txtbiglietto=tk.Entry(fg="yellow",bg="blue",width=50).pack()
+
+lbleta=tk.Label(text=Età,
+        fg="white",
+        bg="red").pack()
+
+txteta=tk.Entry(fg="yellow",bg="blue",width=50).pack()
+
+btncalcolo=tk.Button(text="Calcola",
+            fg="white",
+            bg="red",
+            height=10,
+            width=30).pack()
